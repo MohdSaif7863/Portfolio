f1804f4416c36afe63d054eb21afa17ff55bf271
MohdSaif7863 <mohdsaif5725@gmail.com>
2025-01-16 23:55:37 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
-// import { defineConfig } from "vite";
-
-// export default defineConfig({
-//   root: ".",
-//   server: {
-//     port: 3000, // Adjust port if needed
-//   },
-// });
+});
